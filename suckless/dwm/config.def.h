e0a694577e1d64493c5e05a6f5c34422909693fa
Andrey Maryanenko <a.maryanenko@gmail.com>
2024-06-03 16:03:12 +0300

    Update dwm-config.def.h

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -125,7 +125,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
+	{ MODKEY,                       XK_e,      quit,           {0} },
 };
 
 /* button definitions */
